81a88a8350b8d7942936edc7f337092e8700f531
Ben Hlaban <behlaban@microsoft.com>
2018-06-27 14:03:30 -0500

    WIP

---
+++ ConfigurationDataHelper.ps1
@@ -0,0 +1,15 @@
+﻿$configData = @{
+    AllNodes = @(
+        @{
+            NodeName = "*";
+            DomainName = "seicdevops.local";
+        },
+        @{
+            NodeName = "dc01";
+        }
+    )
+}
+
+$jsonData = ($configData | ConvertTo-Json -Compress).ToString().Replace("`"", "\`"")
+
+Write-Host "[concat('$jsonData')]"
