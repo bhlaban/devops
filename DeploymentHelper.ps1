a5836ce711ee374cf10441e700239fc5953b22ca
Ben Hlaban <behlaban@microsoft.com>
2018-06-27 16:15:56 -0500

    WIP

---
+++ DeploymentHelper.ps1
@@ -0,0 +1,25 @@
+﻿Login-AzureRmAccount -Environment AzureUSGovernment
+
+$location = 'usgovvirginia'
+$resourceGroupName = 'seicdevops-rg'
+$domainName = 'seicdevops.local'
+$adminUsername = 'superuser'
+$adminPassword = ConvertTo-SecureString 'P@$$word12345' -AsPlainText -Force
+$compileDateTime = Get-Date -Format g
+
+Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+
+if ($notPresent)
+{
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+}
+
+$templateUri = $(Get-GitHubRawPath -File .\azuredeploy.json)
+
+$templateParameterObject = @{}
+$templateParameterObject.Add("domainName", $domainName)
+$templateParameterObject.Add("adminUsername", $adminUsername)
+$templateParameterObject.Add("adminPassword", $adminPassword)
+$templateParameterObject.Add("compileDateTime", $compileDateTime)
+
+New-AzureRmResourceGroupDeployment -Name seicdevops-deploy -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -TemplateParameterObject $templateParameterObject
