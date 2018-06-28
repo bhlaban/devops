13bb3a08730ab8ca6312793473b04dd2bfee8d90
Ben Hlaban <behlaban@microsoft.com>
2018-06-28 10:18:14 -0500

    WIP

--- dsc/SqlServerConfig.ps1
+++ dsc/SqlServerConfig.ps1
@@ -2,7 +2,7 @@ configuration SqlServerConfig
 {
     $domainCredential = Get-AutomationPSCredential -Name "DomainCredential"
 
-    Import-DscResource -ModuleName @{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},@{ModuleName='SqlServerDsc';ModuleVersion='11.3.0.0'},'PSDesiredStateConfiguration'
+    Import-DscResource -ModuleName @{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},@{ModuleName='NetworkingDsc';ModuleVersion='6.0.0.0'},@{ModuleName='SqlServerDsc';ModuleVersion='11.3.0.0'},'PSDesiredStateConfiguration'
 
     Node $AllNodes.NodeName
     {
@@ -11,6 +11,19 @@ configuration SqlServerConfig
             Name = $Node.NodeName
             DomainName = $Node.DomainName
             Credential = $domainCredential
+        }
+
+        Firewall DatabaseEngineFirewallRule
+        {
+            Name = "SQL-Server-Database-Engine-TCP-In"
+            DisplayName = "SQL Server Database Engine (TCP-In)"
+            Group = "SQL Server"
+            Ensure = "Present"
+            Enabled = "True"
+            Direction = "Inbound"
+            LocalPort = "1433"
+            Protocol = "TCP"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Engine."
         }
     }
 }
