68d271e3b8a91cf708df85f07b2d7392045a3ba1
Ben Hlaban <behlaban@microsoft.com>
2018-06-27 23:34:12 -0500

    WIP

--- dsc/JumpBoxConfig.ps1
+++ dsc/JumpBoxConfig.ps1
@@ -1,9 +1,15 @@
 configuration JumpBoxConfig
 {
-    Import-DscResource -ModuleName @{ModuleName='cChoco';ModuleVersion='2.3.1.0'},'PSDesiredStateConfiguration'
+    Import-DscResource -ModuleName @{ModuleName='cChoco';ModuleVersion='2.3.1.0'},@{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},'PSDesiredStateConfiguration'
 
     Node $AllNodes.NodeName
     {
+        PowerShellExecutionPolicy ExecutionPolicy
+        {
+            ExecutionPolicyScope = 'LocalMachine'
+            ExecutionPolicy      = 'RemoteSigned'
+        }
+
         cChocoInstaller InstallChocolatey
         {
             InstallDir = "C:\choco"
