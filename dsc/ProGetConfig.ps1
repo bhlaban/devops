42bfadf02da2d84ebf88ed84659c958e7c28abc8
Ben Hlaban <behlaban@microsoft.com>
2018-06-29 12:27:49 -0500

    Added ProGet infrastructure

---
+++ dsc/ProGetConfig.ps1
@@ -0,0 +1,16 @@
+configuration ProGetConfig
+{
+    $domainCredential = Get-AutomationPSCredential -Name "DomainCredential"
+
+    Import-DscResource -ModuleName @{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},'PSDesiredStateConfiguration'
+
+    Node $AllNodes.NodeName
+    {
+        Computer JoinDomain
+        {
+            Name = $Node.NodeName
+            DomainName = $Node.DomainName
+            Credential = $domainCredential
+        }
+    }
+}
