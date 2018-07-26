211ad11faaf93c0c722f010a61c655cb85b4356d
Ben Hlaban <behlaban@microsoft.com>
2018-07-26 18:23:03 -0500

    Tweaked TFS config

--- dsc/TfsConfig.ps1
+++ dsc/TfsConfig.ps1
@@ -1,11 +1,17 @@
 configuration TfsConfig
 {
+    $tfsDownloadLinks = @{
+        "TFS2018" = "https://go.microsoft.com/fwlink/?LinkId=856344"
+        "TFS2017Update2" = "https://go.microsoft.com/fwlink/?LinkId=850949"
+        "TFS2017Update3" = "https://go.microsoft.com/fwlink/?LinkId=857134"
+    }
+    $tfsVersion = "TFS2018"
+    $tfsDownloadLink = $tfsDownloadLinks[$tfsVersion]
+    $tfsInstaller = $env:TEMP + "\tfs_installer.exe"
+
     $domainCredential = Get-AutomationPSCredential -Name "DomainCredential"
 
     Import-DscResource -ModuleName @{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},'PSDesiredStateConfiguration'
-
-    $tfsDownload = "https://go.microsoft.com/fwlink/?LinkId=856344"
-    $tfsInstaller = $env:TEMP + "\tfs_installer.exe"
 
     Node $AllNodes.NodeName
     {
@@ -18,16 +24,15 @@ configuration TfsConfig
 
         Script DownloadTFS
         {
-            GetScript = {
+            GetScript = { 
                 return @{ 'Result' = $true }
             }
             SetScript = {
-                Write-Host "Downloading TFS: " + $tfsInstaller
-                $webClient = New-Object System.Net.WebClient
-                $webClient.DownloadFile($tfsDownload, $tfsInstaller)
+                Write-Host "Downloading TFS: " + $using:tfsDownloadLink
+                Invoke-WebRequest -Uri $using:tfsDownloadLink -OutFile $using:tfsInstaller
             }
             TestScript = {
-                Test-Path $tfsInstaller
+                Test-Path $using:tfsInstaller
             }
 			DependsOn = "[Computer]JoinDomain"
         }
