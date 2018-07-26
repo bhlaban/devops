65790ac2429108c0ae9704b8ea957de265050574
Ben Hlaban <behlaban@microsoft.com>
2018-07-26 07:37:21 -0500

    Added TFS download to TFS config

--- dsc/TfsConfig.ps1
+++ dsc/TfsConfig.ps1
@@ -4,6 +4,9 @@ configuration TfsConfig
 
     Import-DscResource -ModuleName @{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},'PSDesiredStateConfiguration'
 
+    $downloadLink = "https://go.microsoft.com/fwlink/?LinkId=856344"
+    $installerDownload = $env:TEMP + "\tfs_installer.exe"
+
     Node $AllNodes.NodeName
     {
         Computer JoinDomain
@@ -11,6 +14,21 @@ configuration TfsConfig
             Name = $Node.NodeName
             DomainName = $Node.DomainName
             Credential = $domainCredential
+        }
+
+        Script DownloadTFS
+        {
+            GetScript = {
+                return @{ 'Result' = $true }
+            }
+            SetScript = {
+                Write-Host "Downloading TFS: " + $using:downloadLink
+                Invoke-WebRequest -Uri $using:downloadLink -OutFile $using:installerDownload
+            }
+            TestScript = {
+                Test-Path $using:installerDownload
+            }
+			DependsOn = "[Computer]JoinDomain"
         }
     }
 }
