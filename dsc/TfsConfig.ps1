b63b52de69a6132132dbcc63040b2902c2a2e01a
Ben Hlaban <behlaban@microsoft.com>
2018-07-27 14:04:20 -0500

    Updated TFS config

--- dsc/TfsConfig.ps1
+++ dsc/TfsConfig.ps1
@@ -1,40 +1,33 @@
 configuration TfsConfig
 {
-    $tfsDownloadLinks = @{
-        "TFS2018" = "https://go.microsoft.com/fwlink/?LinkId=856344"
-        "TFS2017Update2" = "https://go.microsoft.com/fwlink/?LinkId=850949"
-        "TFS2017Update3" = "https://go.microsoft.com/fwlink/?LinkId=857134"
-    }
-    $tfsVersion = "TFS2018"
-    $tfsDownloadLink = $tfsDownloadLinks[$tfsVersion]
-    $tfsInstaller = $env:TEMP + "\tfs_installer.exe"
-
     $domainCredential = Get-AutomationPSCredential -Name "DomainCredential"
 
-    Import-DscResource -ModuleName @{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},'PSDesiredStateConfiguration'
+    Import-DscResource -ModuleName @{ModuleName = 'ComputerManagementDsc'; ModuleVersion = '5.1.0.0'}, 'PSDesiredStateConfiguration'
 
     Node $AllNodes.NodeName
     {
-        Computer JoinDomain
-        {
-            Name = $Node.NodeName
+        Computer JoinDomain {
+            Name       = $Node.NodeName
             DomainName = $Node.DomainName
             Credential = $domainCredential
         }
 
-        Script DownloadTFS
-        {
-            GetScript = { 
-                return @{ 'Result' = $true }
+        Script DownloadTFS {
+            GetScript = {
+                @{
+                    GetScript = $GetScript
+                    SetScript = $SetScript
+                    TestScript = $TestScript
+                    Result = ('True' -in (Test-Path C:\Installs\tfs-install.msi))
+                }
             }
             SetScript = {
-                Write-Host "Downloading TFS: " + $using:tfsDownloadLink
-                Invoke-WebRequest -Uri $using:tfsDownloadLink -OutFile $using:tfsInstaller
+                Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?LinkId=856344" -OutFile "C:\Installs\tfs-install.msi"
             }
             TestScript = {
-                Test-Path $using:tfsInstaller
+                $Status = ('True' -in (Test-Path C:\Installs\tfs-install.msi))
+                $Status -eq $True
             }
-			DependsOn = "[Computer]JoinDomain"
         }
     }
 }
