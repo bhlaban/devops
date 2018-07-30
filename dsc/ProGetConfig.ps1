bbf0715351ef5be29426ee3f8987a072639699c7
Ben Hlaban <behlaban@microsoft.com>
2018-07-30 12:19:48 -0500

    Added ProGet download and TFS install

--- dsc/ProGetConfig.ps1
+++ dsc/ProGetConfig.ps1
@@ -1,6 +1,9 @@
 configuration ProGetConfig
 {
     $domainCredential = Get-AutomationPSCredential -Name "DomainCredential"
+    $installsDirectory = "C:\Installs"
+    $proGetDownload = "https://inedo.com/proget/download/sql/5.1.6"
+    $proGetInstaller = Join-Path $installsDirectory "ProGetSetup5.1.6.exe"
 
     Import-DscResource -ModuleName @{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},'PSDesiredStateConfiguration'
 
@@ -11,6 +14,31 @@ configuration ProGetConfig
             Name = $Node.NodeName
             DomainName = $Node.DomainName
             Credential = $domainCredential
+        }
+
+        File InstallsDirectory {
+            Ensure = "Present"
+            Type = "Directory"
+            DestinationPath = $installsDirectory
+        }
+
+        Script DownloadProGet {
+            GetScript = {
+                @{
+                    GetScript = $GetScript
+                    SetScript = $SetScript
+                    TestScript = $TestScript
+                    Result = ('True' -in (Test-Path $using:proGetInstaller))
+                }
+            }
+            SetScript = {
+                Invoke-WebRequest -Uri $using:proGetDownload -OutFile $using:proGetInstaller
+            }
+            TestScript = {
+                $Status = ('True' -in (Test-Path $using:proGetInstaller))
+                $Status -eq $True
+            }
+            DependsOn = "[File]InstallsDirectory"
         }
     }
 }
--- dsc/TfsConfig.ps1
+++ dsc/TfsConfig.ps1
@@ -3,7 +3,9 @@ configuration TfsConfig
     $domainCredential = Get-AutomationPSCredential -Name "DomainCredential"
     $tfsDownload = "https://go.microsoft.com/fwlink/?LinkId=856344"
     $installsDirectory = "C:\Installs"
-    $tfsInstaller = Join-Path $installsDirectory "tfs-installer.exe"
+    $tfsInstallFile = Join-Path $installsDirectory "tfs-install.exe"
+    $tfsInstallLog = Join-Path $installsDirectory "tfs-install-log.txt"
+    $tfsConfigExe = "C:\Program Files\Microsoft Team Foundation Server 2018\Tools\TfsConfig.exe"
 
     Import-DscResource -ModuleName @{ModuleName='ComputerManagementDsc';ModuleVersion='5.1.0.0'},'PSDesiredStateConfiguration'
 
@@ -28,17 +30,41 @@ configuration TfsConfig
                     GetScript = $GetScript
                     SetScript = $SetScript
                     TestScript = $TestScript
-                    Result = ('True' -in (Test-Path $using:tfsInstaller))
+                    Result = ('True' -in (Test-Path $using:tfsInstallFile))
                 }
             }
             SetScript = {
-                Invoke-WebRequest -Uri $using:tfsDownload -OutFile $using:tfsInstaller
+                Invoke-WebRequest -Uri $using:tfsDownload -OutFile $using:tfsInstallFile
             }
             TestScript = {
-                $Status = ('True' -in (Test-Path $using:tfsInstaller))
+                $Status = ('True' -in (Test-Path $using:tfsInstallFile))
                 $Status -eq $True
             }
             DependsOn = "[File]InstallsDirectory"
+        }
+
+        Script InstallTFS
+        {
+            GetScript = { 
+                @{
+                    GetScript = $GetScript
+                    SetScript = $SetScript
+                    TestScript = $TestScript
+                    Result = ('True' -in (Test-Path $using:tfsConfigExe))
+                }
+            }
+            SetScript = {
+                $cmd = $using:tfsInstaller + " /full /quiet /Log $using:tfsInstallLog"
+                Invoke-Expression $cmd | Write-Verbose
+                #Sleep for 10 seconds to make sure installer is going
+                Start-Sleep -s 10
+                #The tfs installer will per default run in the background. We will wait for it. 
+                Wait-Process -Name "tfs_installer"
+            }
+            TestScript = {
+                Test-Path $using:tfsConfigExe
+            }
+            DependsOn = "[Script]DownloadTFS"
         }
     }
 }
