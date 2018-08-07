9d5cf7e4ca223b8060249b3d37577fac86284e88
Ben Hlaban <behlaban@microsoft.com>
2018-08-07 10:15:40 -0500

    Added scriptlets to delete Azure Automation DSC resources.

--- DeleteVmHelper.ps1
+++ DeleteVmHelper.ps1
@@ -1,6 +1,6 @@
 ﻿#Login-AzureRmAccount -Environment AzureUSGovernment
 
-$vmsToDelete = @("jama01", "proget01", "tfs01", "tfsagent01")
+$servicesToDelete = @("Tfs", "TfsAgent")
 
 function Delete-VM {
     
@@ -8,11 +8,41 @@ function Delete-VM {
         [Parameter(Mandatory=$true, Position=0)]
         [string] $resourceGroupName,
         [Parameter(Mandatory=$true, Position=1)]
-        [string] $vmName
+        [string] $automationAccountName,
+        [Parameter(Mandatory=$true, Position=2)]
+        [string] $serviceName
     )
 
-    $diskName = $vmName + '-disk-os'
-    $nicName = $vmName + '-nic'
+    $vmName = $serviceName + '01'
+    $configurationName = $serviceName + 'Config'
+
+    $dscNode = Get-AzureRmAutomationDscNode -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name $vmName -ErrorAction Ignore
+    If($dscNode -ne $null) {
+        Write-Host "Deleting $vmName DSC node."
+        Unregister-AzureRmAutomationDscNode -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Id $dscNode.Id
+        Write-Host "$vmName DSC node deleted."
+    }
+    else {
+        Write-Host "$vmName DSC node does not exist."
+    }
+
+    $nodeConfigurations = Get-AzureRmAutomationDscNodeConfiguration -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -ErrorAction Ignore
+    foreach ($nodeConfiguration in $nodeConfigurations) {
+	    if ($nodeConfiguration.ConfigurationName.Contains($configurationName)) {
+            Write-Host "Deleting" $nodeConfiguration.Name "DSC node configuration"
+            Remove-AzureRmAutomationDscNodeConfiguration -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name $nodeConfiguration.Name -IgnoreNodeMappings -Force
+            Write-Host $nodeConfiguration.Name "DSC node configuration deleted."
+        }
+    }
+
+    If(Get-AzureRmAutomationDscConfiguration -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name $configurationName -ErrorAction Ignore) {
+        Write-Host "Deleting $configurationName DSC configuration."
+        Remove-AzureRmAutomationDscConfiguration -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -Name $configurationName -Force
+        Write-Host "$configurationName DSC configuration deleted."
+    }
+    else {
+        Write-Host "$configurationName DSC configuration does not exist."
+    }
 
     If(Get-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName -ErrorAction Ignore) {
         Write-Host "Deleting $vmName."
@@ -23,6 +53,7 @@ function Delete-VM {
         Write-Host "$vmName does not exist."
     }
 
+    $diskName = $vmName + '-disk-os'
     If(Get-AzureRmDisk -ResourceGroupName $resourceGroupName -Name $diskName -ErrorAction Ignore) {
         Write-Host "Deleting $diskName."
         Remove-AzureRmDisk -ResourceGroupName $resourceGroupName -DiskName $diskName -Force
@@ -32,7 +63,7 @@ function Delete-VM {
         Write-Host "$diskName does not exist."
     }
 
-
+    $nicName = $vmName + '-nic'
     If(Get-AzureRmNetworkInterface -ResourceGroupName $resourceGroupName -Name $nicName -ErrorAction Ignore) {
         Write-Host "Deleting $nicName."
         Remove-AzureRmNetworkInterface -ResourceGroupName $resourceGroupName -Name $nicName -Force
@@ -41,11 +72,11 @@ function Delete-VM {
     else {
         Write-Host "$nicName does not exist."
     }
-
 }
 
 $resourceGroupName = 'devops-rg'
+$automationAccountName = 'automation'
 
-foreach ($vmToDelete in $vmsToDelete) {
-	Delete-VM -resourceGroupName $resourceGroupName -vmName $vmToDelete
+foreach ($serviceToDelete in $servicesToDelete) {
+	Delete-VM -resourceGroupName $resourceGroupName -automationAccountName $automationAccountName -serviceName $serviceToDelete
 }
