2bc2e8e2175b663238c501c9b6a323cb3a7ca9c3
Ben Hlaban <behlaban@microsoft.com>
2018-08-10 18:30:48 -0500

    WIP

--- dsc/JamaConfig.ps1
+++ dsc/JamaConfig.ps1
@@ -1,9 +1,16 @@
 configuration JamaConfig
 {
+    Import-DscResource -ModuleName 'nx'
     Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
 
     Node $AllNodes.NodeName
     {
-
+        nxFile ExampleFile
+        {
+            DestinationPath = "/tmp/example"
+            Contents = "hello world"
+            Ensure = "Present"
+            Type = "File"
+        }
     }
 }
--- dsc/JumpBoxConfig.ps1
+++ dsc/JumpBoxConfig.ps1
@@ -1,9 +1,13 @@
 configuration JumpBoxConfig
 {
+    Import-DscResource -ModuleName 'cChoco'
     Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
 
     Node $AllNodes.NodeName
     {
-
+        cChocoInstaller InstallChoco
+        {
+            InstallDir = "c:\choco"
+        }
     }
 }
